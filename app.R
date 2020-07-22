0f892a0286f49ea4736f32d719fc72c0a0700782
Thanasis Vogogias <avogogia@ed.ac.uk>
2020-07-22 13:06:05 +0300

    Plot shown within dynamically generated tab

--- app.R
+++ app.R
@@ -13,62 +13,72 @@ ui <- fluidPage(title = "Cycle Scenarios",
                 titlePanel("Cycle Scenarios"),            
                 
                 sidebarLayout(
-                    
-                    sidebarPanel(
-
-                        tags$h4("Variance"),
-                        # Set Genetic Variance 
-                        numericInput("varG", "Genetic:",
-                                     min = 0, max = 100, value = 1, step = 0.1, width = '80px'), 
-                        # Add tooltip with instructions/info
-                        bsTooltip("varG", "Genetic variance. The variance between entries in the first stage of yield trials.",
-                                  "right", "hover", NULL),
-                        # Set GxL(Y) Variance 
-                        numericInput("varGxL", "GxL(Y):",
-                                     min = 0, max = 100, value = 1, step = 0.1, width = '80px'), 
-                        # Add tooltip with instructions/info
-                        bsTooltip("varGxL", "Genotype-by-location nested in year interaction variance. This value is equivalent to the sum of genotype-by-location interaction variance and genotype-by-location-by-year interaction varaince.",
-                                  "right", "hover", NULL),
-                        # Set GxY Variance 
-                        numericInput("varGxY", "GxY:",
-                                     min = 0, max = 100, value = 1, step = 0.1, width = '80px'), 
-                        # Add tooltip with instructions/info
-                        bsTooltip("varGxY", "Genotype-by-year interaction variance.",
-                                  "right", "hover", NULL),
-                        
-                        numericInput("negen", "Early Generations",
-                                     min = 0, max = 100, value = 4, step = 1, width = '80px'), 
-                        # Add tooltip with instructions/info
-                        bsTooltip("negen", "GNumber of early generation years. This phase of the breeding program is modeled without selection.",
-                                  "right", "hover", NULL),
-                        
-                        tags$h4("Yield Trials"),
-                        DT::DTOutput("stages_table"),
-                        # Add tooltip with instructions/info
-                        # bsTooltip("stages_table", "Number of entries. Must be smaller than or equal to the number of entries in the previous stage.
-                        #                           Number of years. Increasing this value will increase heritability by decreasing variation due to GxY, GxL(Y) and plot error.
-                        #                           Number of locations. Increasing this value will increase heritability by decreasing variation due to GxL(Y) and plot error.
-                        #                           Number of replications. Increasing this value will increase heritability by decreasing variation due to plot error.",
-                        #                           "right", "hover", NULL),
-                        actionButton("add_btn", "Add"),
-                        actionButton("delete_btn", "Delete"),
-    
                   
-                        numericInput("varieties", "Varieties",
-                                     min = 1, max = 10, value = 1, step = 1, width = '80px'), 
-                        # Add tooltip with instructions/info
-                        bsTooltip("varieties", "The final number of selected entries. Must be smaller than or equal to the number of entries in the last stage.",
-                                  "right", "hover", NULL),
-                                            
-                        actionButton("run_btn", "Run"),
-                        
-
-                        
-                    ), # endof SidebarPanel
-                    # Show plots and charts 
-                    mainPanel(
-                      uiOutput('mytabs')
-                    ) # endof mainPanel
+                  sidebarPanel(
+                    
+                    tags$h4("Variance"),
+                    
+                    bootstrapPage(
+                      div(style="display:inline-block",numericInput("varG", "Genetic:",
+                                                                    min = 0, max = 100, value = 1, step = 0.1, width = '80px')),
+                      div(style="display:inline-block",numericInput("varGxL", "GxL(Y):",
+                                                                    min = 0, max = 100, value = 1, step = 0.1, width = '80px')),
+                      div(style="display:inline-block",numericInput("varGxY", "GxY:",
+                                                                    min = 0, max = 100, value = 1, step = 0.1, width = '80px'))
+                    ),
+                    
+                    # Set Genetic Variance 
+                    # TV numericInput("varG", "Genetic:",
+                    # TV              min = 0, max = 100, value = 1, step = 0.1, width = '80px'), 
+                    # Add tooltip with instructions/info
+                    bsTooltip("varG", "Genetic variance. The variance between entries in the first stage of yield trials.",
+                              "right", "hover", NULL),
+                    # Set GxL(Y) Variance 
+                    # TV numericInput("varGxL", "GxL(Y):",
+                    # TV              min = 0, max = 100, value = 1, step = 0.1, width = '80px'), 
+                    # Add tooltip with instructions/info
+                    bsTooltip("varGxL", "Genotype-by-location nested in year interaction variance. This value is equivalent to the sum of genotype-by-location interaction variance and genotype-by-location-by-year interaction varaince.",
+                              "right", "hover", NULL),
+                    # Set GxY Variance 
+                    # TV numericInput("varGxY", "GxY:",
+                    # TV              min = 0, max = 100, value = 1, step = 0.1, width = '80px'), 
+                    # Add tooltip with instructions/info
+                    bsTooltip("varGxY", "Genotype-by-year interaction variance.",
+                              "right", "hover", NULL),
+                    
+                    numericInput("negen", "Early Generations",
+                                 min = 0, max = 100, value = 4, step = 1, width = '80px'), 
+                    # Add tooltip with instructions/info
+                    bsTooltip("negen", "GNumber of early generation years. This phase of the breeding program is modeled without selection.",
+                              "right", "hover", NULL),
+                    
+                    tags$h4("Yield Trials"),
+                    DT::DTOutput("stages_table"),
+                    # Add tooltip with instructions/info
+                    # bsTooltip("stages_table", "Number of entries. Must be smaller than or equal to the number of entries in the previous stage.
+                    #                           Number of years. Increasing this value will increase heritability by decreasing variation due to GxY, GxL(Y) and plot error.
+                    #                           Number of locations. Increasing this value will increase heritability by decreasing variation due to GxL(Y) and plot error.
+                    #                           Number of replications. Increasing this value will increase heritability by decreasing variation due to plot error.",
+                    #                           "right", "hover", NULL),
+                    actionButton("add_btn", "Add"),
+                    actionButton("delete_btn", "Delete"),
+                    
+                    
+                    numericInput("varieties", "Varieties",
+                                 min = 1, max = 10, value = 1, step = 1, width = '80px'), 
+                    # Add tooltip with instructions/info
+                    bsTooltip("varieties", "The final number of selected entries. Must be smaller than or equal to the number of entries in the last stage.",
+                              "right", "hover", NULL),
+                    
+                    actionButton("run_btn", "Run"),
+                    
+                    
+                    
+                  ), # endof SidebarPanel
+                  # Show plots and charts 
+                  mainPanel(
+                    uiOutput('mytabs')
+                  ) # endof mainPanel
                 ) # endof sidebarLayout
                 
 )
@@ -99,12 +109,12 @@ server <- function(input, output, clientData, session) {
   #TV                                     selection = "none",
   #TV                                     editable = list(target = "cell", disable = list(columns = 0)),
   #TV                                     server = FALSE)
-
+  
   
   # Using reactiveVal to add a server side variable observable and mutable at the same time
   #ytiR <- reactiveVal(yti)
   yti <- reactiveValues(data = yt)
-
+  
   
   proxy = dataTableProxy('stages_table')
   observeEvent(input$stages_table_cell_edit, {
@@ -134,12 +144,12 @@ server <- function(input, output, clientData, session) {
                                      selection = "none",
                                      editable = list(target = "cell", disable = list(columns = 0)),
                                      server = FALSE)
-    
+  
   # Observe Button Clicks for adding or removing rows (stages) from the DT
   observeEvent(input$add_btn, {
-
+    
     yti$data = rbind(yti$data, c(length(yti$data[,1])+1,2,1,1,1,1))
-     
+    
     # Render the manipulated table in Shiny after row add
     output$stages_table = DT::renderDT(yti$data, 
                                        class = "cell-border, compact, hover", 
@@ -171,10 +181,10 @@ server <- function(input, output, clientData, session) {
                                        server = FALSE)
   })
   
-
+  
   # Render the manipulated table in Shiny  
- # output$stages_table = DT::renderDT(ytiR(), server = FALSE)
-    
+  # output$stages_table = DT::renderDT(ytiR(), server = FALSE)
+  
   
   observeEvent(input$run_btn, {
     
@@ -186,8 +196,8 @@ server <- function(input, output, clientData, session) {
       herTabs = lapply(1: nTabs, function(i){
         tabPanel(paste('Scenario', sep = " ", i),
                  plotOutput(paste('cyPlot', sep = "", i))
-                 )
-        }) 
+        )
+      }) 
       
       
       # TV do.call(tabsetPanel, myTabs)
@@ -217,7 +227,7 @@ server <- function(input, output, clientData, session) {
               xlab="Stage",
               ylab="Mean Genetic Value")
     })   # end of renderPlot
-
+    
     
     # TV WORKS    output$cyPlot1 <- nplot
     #
@@ -248,10 +258,10 @@ server <- function(input, output, clientData, session) {
     
   }) # end of run button
   
- 
-
-    
-    
+  
+  
+  
+  
 }
 
 # Run the application 
